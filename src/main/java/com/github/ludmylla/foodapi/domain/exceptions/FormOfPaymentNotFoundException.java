9d4b3c70a14a6523b44b2f5fc603bf806eab91e4
Ludmylla <ludmay4@gmail.com>
2022-08-04 17:44:10 -0300

    criando exception para forma de pagamento

---
+++ src/main/java/com/github/ludmylla/foodapi/domain/exceptions/FormOfPaymentNotFoundException.java
@@ -0,0 +1,17 @@
+package com.github.ludmylla.foodapi.domain.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class FormOfPaymentNotFoundException extends EntityNotFoundException{
+    private static final long serialVersionUID = 1L;
+
+    public FormOfPaymentNotFoundException(String message){
+        super(message);
+    }
+
+    public FormOfPaymentNotFoundException(Long id) {
+        this(String.format("There is no code Form of payment %d" ,id));
+    }
+}
