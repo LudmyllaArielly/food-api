9432a52462ac1bfce89b9e57bb0d5a516fbbf06c
Ludmylla <ludmay4@gmail.com>
2022-08-04 14:46:48 -0300

    refatorando controller de cozinha para usar dto

--- src/main/java/com/github/ludmylla/foodapi/api/controller/KitchenController.java
+++ src/main/java/com/github/ludmylla/foodapi/api/controller/KitchenController.java
@@ -1,5 +1,9 @@
 package com.github.ludmylla.foodapi.api.controller;
 
+import com.github.ludmylla.foodapi.api.assembler.KitchenInputDisassembler;
+import com.github.ludmylla.foodapi.api.assembler.KitchenModelAssembler;
+import com.github.ludmylla.foodapi.api.model.dtos.KitchenModel;
+import com.github.ludmylla.foodapi.api.model.dtos.input.KitchenInputModel;
 import com.github.ludmylla.foodapi.domain.model.Kitchen;
 import com.github.ludmylla.foodapi.domain.service.KitchenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,28 +22,36 @@ public class KitchenController {
     @Autowired
     private KitchenService service;
 
+    @Autowired
+    private KitchenInputDisassembler kitchenInputDisassembler;
+
+    @Autowired
+    private KitchenModelAssembler kitchenModelAssembler;
+
     @PostMapping
-    public ResponseEntity<Kitchen> create(@RequestBody @Valid Kitchen kitchen){
-        Kitchen kitchens = service.create(kitchen);
-        return ResponseEntity.status(HttpStatus.CREATED).body(kitchens);
+    public ResponseEntity<KitchenModel> create(@RequestBody @Valid KitchenInputModel kitchenInput){
+        Kitchen kitchen = kitchenInputDisassembler.toDomainModel(kitchenInput);
+        Kitchen kitchenCreate = service.create(kitchen);
+        return ResponseEntity.status(HttpStatus.CREATED).body(kitchenModelAssembler.toModel(kitchenCreate));
     }
 
     @GetMapping
-    public ResponseEntity<List<Kitchen>> findAll(){
+    public ResponseEntity<List<KitchenModel>> findAll(){
         List<Kitchen> list = service.findAll();
-        return ResponseEntity.ok(list);
+        return ResponseEntity.ok(kitchenModelAssembler.toCollectionModel(list));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Kitchen> findById(@PathVariable Long id){
+    public ResponseEntity<KitchenModel> findById(@PathVariable Long id){
         Kitchen kitchen = service.findById(id);
-        return ResponseEntity.ok(kitchen);
+        return ResponseEntity.ok(kitchenModelAssembler.toModel(kitchen));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Kitchen> update(@PathVariable Long id, @RequestBody @Valid Kitchen kitchen){
+    public ResponseEntity<KitchenModel> update(@PathVariable Long id, @RequestBody @Valid KitchenInputModel kitchenInput){
+        Kitchen kitchen = kitchenInputDisassembler.toDomainModel(kitchenInput);
         Kitchen kitchenUpdate = service.update(kitchen,id);
-        return ResponseEntity.ok(kitchenUpdate);
+        return ResponseEntity.ok(kitchenModelAssembler.toModel(kitchenUpdate));
     }
 
     @DeleteMapping("/{id}")
