41482d64d0ed51934522fa7172fa2f0f81121bbc
Ludmylla <ludmay4@gmail.com>
2022-08-07 12:07:59 -0300

    criando método e endpoints de produtos

---
+++ src/main/java/com/github/ludmylla/foodapi/api/controller/RestaurantProductController.java
@@ -0,0 +1,59 @@
+package com.github.ludmylla.foodapi.api.controller;
+
+import com.github.ludmylla.foodapi.api.assembler.ProductInputDisassembler;
+import com.github.ludmylla.foodapi.api.assembler.ProductModelAssembler;
+import com.github.ludmylla.foodapi.domain.dtos.ProductModel;
+import com.github.ludmylla.foodapi.domain.dtos.input.ProductInputModel;
+import com.github.ludmylla.foodapi.domain.model.Product;
+import com.github.ludmylla.foodapi.domain.service.ProductService;
+import com.github.ludmylla.foodapi.domain.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurants/{restaurantId}/products")
+public class RestaurantProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
+    private ProductModelAssembler productModelAssembler;
+
+    @Autowired
+    private ProductInputDisassembler productInputDisassembler;
+
+    @PostMapping
+    public ResponseEntity<ProductModel> create(@PathVariable Long restaurantId, @RequestBody @Valid ProductInputModel productInput){
+        Product product = productInputDisassembler.toDomainModel(productInput);
+        product = productService.create(restaurantId, product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productModelAssembler.toModel(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductModel>> findAllByRestaurant(@PathVariable Long restaurantId){
+        List<Product> list = productService.findAllByRestaurant(restaurantId);
+        return ResponseEntity.ok(productModelAssembler.toCollectionModel(list));
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductModel> findById(@PathVariable Long restaurantId, @PathVariable Long productId){
+        Product product = productService.findByIdRestaurant(restaurantId, productId);
+        return ResponseEntity.ok(productModelAssembler.toModel(product));
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<ProductModel> update(@PathVariable Long restaurantId, @PathVariable Long productId, @RequestBody @Valid ProductInputModel productInput){
+        Product product = productInputDisassembler.toDomainModel(productInput);
+        product = productService.update(productId, restaurantId, product);
+        return ResponseEntity.ok(productModelAssembler.toModel(product));
+    }
+}
---
+++ src/main/java/com/github/ludmylla/foodapi/domain/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.github.ludmylla.foodapi.domain.service;
+
+import com.github.ludmylla.foodapi.domain.model.Product;
+import com.github.ludmylla.foodapi.domain.model.Restaurant;
+import com.github.ludmylla.foodapi.domain.repository.ProductRepository;
+import com.github.ludmylla.foodapi.domain.service.exceptions.ProductNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+
+    @Transactional
+    public Product create(Long restaurantId,Product product){
+        Restaurant restaurant = restaurantService.findById(restaurantId);
+        product.setRestaurant(restaurant);
+        return productRepository.save(product);
+    }
+
+    public List<Product> findAllByRestaurant(Long restaurantId){
+        Restaurant restaurant = restaurantService.findById(restaurantId);
+        return productRepository.findByRestaurant(restaurant);
+    }
+
+    public Product findByIdRestaurant(Long restaurantId, Long productId){
+        return productRepository.findById(productId, restaurantId)
+                .orElseThrow(() -> new ProductNotFoundException(productId, restaurantId));
+    }
+
+    @Transactional
+    public Product update(Long productId, Long restaurantId, Product product){
+        Product productActual = findByIdRestaurant(restaurantId, productId);
+        product.setId(productActual.getId());
+        return productRepository.save(product);
+    }
+}
