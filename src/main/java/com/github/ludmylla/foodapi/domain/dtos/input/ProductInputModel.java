df4b68529a2ac63772ef795a4f1ab7afc94c614e
Ludmylla <ludmay4@gmail.com>
2022-08-07 10:08:07 -0300

    criando dtos para produto

---
+++ src/main/java/com/github/ludmylla/foodapi/domain/dtos/input/ProductInputModel.java
@@ -0,0 +1,27 @@
+package com.github.ludmylla.foodapi.domain.dtos.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductInputModel {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal price;
+
+    @NotNull
+    private Boolean active;
+}
