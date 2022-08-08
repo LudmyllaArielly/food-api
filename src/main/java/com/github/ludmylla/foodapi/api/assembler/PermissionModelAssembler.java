c3755a9766467ddd3b99db258f928195b880b520
Ludmylla <ludmay4@gmail.com>
2022-08-07 21:02:12 -0300

    criando assembler para permissão

--- src/main/java/com/github/ludmylla/foodapi/api/assembler/PermissionModelAssembler.java
+++ src/main/java/com/github/ludmylla/foodapi/api/assembler/PermissionModelAssembler.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +20,7 @@ public class PermissionModelAssembler {
         return mapper.map(permission, PermissionModel.class);
     }
 
-    public List<PermissionModel> toCollectionModel(List<Permission> permissions) {
+    public List<PermissionModel> toCollectionModel(Collection<Permission> permissions) {
         return permissions.stream()
                 .map(permission -> toModel(permission))
                 .collect(Collectors.toList());
