e01c2eb8adf124999fe06493cb983d76f636f42f
Ludmylla <ludmay4@gmail.com>
2022-08-10 12:15:22 -0300

    criando dto de resumo para restaurante

---
+++ src/main/java/com/github/ludmylla/foodapi/domain/dtos/RestaurantResumeModel.java
@@ -0,0 +1,12 @@
+package com.github.ludmylla.foodapi.domain.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestaurantResumeModel {
+
+    private Long id;
+    private String name;
+}
