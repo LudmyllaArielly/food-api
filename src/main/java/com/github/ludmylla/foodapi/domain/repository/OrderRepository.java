7927486cec44517924e1d1984dd9356e460a092a
Ludmylla <ludmay4@gmail.com>
2022-08-10 13:34:23 -0300

    Criando método findAll em repositório de pedido para listar tudo em uma consuta só

--- src/main/java/com/github/ludmylla/foodapi/domain/repository/OrderRepository.java
+++ src/main/java/com/github/ludmylla/foodapi/domain/repository/OrderRepository.java
@@ -2,10 +2,14 @@ package com.github.ludmylla.foodapi.domain.repository;
 
 import com.github.ludmylla.foodapi.domain.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-
+    @Query("from Order o join fetch o.user join fetch o.restaurant k join fetch k.kitchen")
+    List<Order> findAll();
 }
