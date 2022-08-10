1d3fa70317631f636e7800fbde51c1480d1621b7
Ludmylla <ludmay4@gmail.com>
2022-08-10 17:45:31 -0300

    adicionando descrição ao status do pedido

--- src/main/java/com/github/ludmylla/foodapi/domain/model/OrderStatus.java
+++ src/main/java/com/github/ludmylla/foodapi/domain/model/OrderStatus.java
@@ -2,5 +2,18 @@ package com.github.ludmylla.foodapi.domain.model;
 
 public enum OrderStatus {
 
-    CREATED, CONFIRMED, DELIVERED, CANCELED;
+    CREATED("Created"),
+    CONFIRMED("Confirmed"),
+    DELIVERED("Delivered"),
+    CANCELED("Canceled");
+
+    private String description;
+
+    OrderStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
 }
