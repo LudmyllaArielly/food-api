7798ccf6cff6005014dfb2a7d6d62b9f4cae35fb
Ludmylla <ludmay4@gmail.com>
2022-08-11 17:13:25 -0300

    criando método para busca por código

--- src/main/java/com/github/ludmylla/foodapi/domain/repository/OrderRepository.java
+++ src/main/java/com/github/ludmylla/foodapi/domain/repository/OrderRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("from Order o join fetch o.user join fetch o.restaurant k join fetch k.kitchen")
     List<Order> findAll();
+
+    Optional<Order> findByCode(String code);
 }
