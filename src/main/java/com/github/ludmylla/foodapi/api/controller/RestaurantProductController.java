483e138c62b6762a880c00736963cd6c35a57949
Ludmylla <ludmay4@gmail.com>
2022-09-01 15:29:26 -0300

    refatorando endpoint de buscar todos os produtos do restaurante

--- src/main/java/com/github/ludmylla/foodapi/api/controller/RestaurantProductController.java
+++ src/main/java/com/github/ludmylla/foodapi/api/controller/RestaurantProductController.java
@@ -32,26 +32,26 @@ public class RestaurantProductController {
     private ProductInputDisassembler productInputDisassembler;
 
     @PostMapping
-    public ResponseEntity<ProductModel> create(@PathVariable Long restaurantId, @RequestBody @Valid ProductInputModel productInput){
+    public ResponseEntity<ProductModel> create(@PathVariable Long restaurantId, @RequestBody @Valid ProductInputModel productInput) {
         Product product = productInputDisassembler.toDomainModel(productInput);
         product = productService.create(restaurantId, product);
         return ResponseEntity.status(HttpStatus.CREATED).body(productModelAssembler.toModel(product));
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductModel>> findAllByRestaurant(@PathVariable Long restaurantId){
-        List<Product> list = productService.findAllByRestaurant(restaurantId);
+    public ResponseEntity<List<ProductModel>> findAllByRestaurant(@PathVariable Long restaurantId, @RequestParam(required = false) boolean includeInactive) {
+        List<Product> list = productService.findAllProductByRestaurant(restaurantId, includeInactive);
         return ResponseEntity.ok(productModelAssembler.toCollectionModel(list));
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<ProductModel> findById(@PathVariable Long restaurantId, @PathVariable Long productId){
+    public ResponseEntity<ProductModel> findById(@PathVariable Long restaurantId, @PathVariable Long productId) {
         Product product = productService.findByIdRestaurant(restaurantId, productId);
         return ResponseEntity.ok(productModelAssembler.toModel(product));
     }
 
     @PutMapping("/{productId}")
-    public ResponseEntity<ProductModel> update(@PathVariable Long restaurantId, @PathVariable Long productId, @RequestBody @Valid ProductInputModel productInput){
+    public ResponseEntity<ProductModel> update(@PathVariable Long restaurantId, @PathVariable Long productId, @RequestBody @Valid ProductInputModel productInput) {
         Product product = productInputDisassembler.toDomainModel(productInput);
         product = productService.update(restaurantId, productId, product);
         return ResponseEntity.ok(productModelAssembler.toModel(product));
