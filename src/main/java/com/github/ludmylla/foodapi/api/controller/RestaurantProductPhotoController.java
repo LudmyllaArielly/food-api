6f45638e1ba4a2e353be28daea9dd8600e7caff6
Ludmylla <ludmay4@gmail.com>
2022-09-15 18:32:44 -0300

    adicionando mais campos no endpoint de enviar foto

--- src/main/java/com/github/ludmylla/foodapi/api/controller/RestaurantProductPhotoController.java
+++ src/main/java/com/github/ludmylla/foodapi/api/controller/RestaurantProductPhotoController.java
@@ -1,5 +1,6 @@
 package com.github.ludmylla.foodapi.api.controller;
 
+import com.github.ludmylla.foodapi.domain.dtos.input.PhotoProductInput;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,17 +13,19 @@ import java.util.UUID;
 public class RestaurantProductPhotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void updatePhoto(@PathVariable Long restaurantId, @PathVariable Long productId, @RequestParam MultipartFile file){
+    public void updatePhoto(@PathVariable Long restaurantId, @PathVariable Long productId,
+                            @RequestParam MultipartFile file, PhotoProductInput photoProductInput){
 
-        var nameFile = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+        var nameFile = UUID.randomUUID().toString() + "_" + photoProductInput.getFile().getOriginalFilename();
 
         var filePhoto = Path.of("C:/Users/ludmy/Music/images", nameFile);
 
         System.out.println(filePhoto);
         System.out.println(file.getContentType());
+        System.out.println(photoProductInput.getDescription());
 
         try {
-            file.transferTo(filePhoto);
+            photoProductInput.getFile().transferTo(filePhoto);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
