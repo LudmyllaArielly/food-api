1ffe1099ea44c86c781348aa19f4d7ac5131045d
Ludmylla <ludmay4@gmail.com>
2022-09-16 16:32:11 -0300

    criando storage properties para armazenas fotos na aws ou disco

---
+++ src/main/java/com/github/ludmylla/foodapi/core/storage/StorageProperties.java
@@ -0,0 +1,37 @@
+package com.github.ludmylla.foodapi.core.storage;
+
+import com.amazonaws.regions.Regions;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Path;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("food.api.storage")
+public class StorageProperties {
+
+    private Local local = new Local();
+    private S3 s3 = new S3();
+
+    @Getter
+    @Setter
+    public class Local {
+
+        private Path folderPhotos;
+    }
+
+    @Getter
+    @Setter
+    public class S3 {
+
+        private String idKeyAccess;
+        private String keyAccessSecret;
+        private String bucket;
+        private Regions region;
+        private String directoryPhotos;
+    }
+}
