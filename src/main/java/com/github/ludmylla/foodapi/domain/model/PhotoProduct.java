f1042f48da361df4274d9f0838dc8b8dbf22bf73
Ludmylla <ludmay4@gmail.com>
2022-09-15 21:22:45 -0300

    criando classe photo_produto

---
+++ src/main/java/com/github/ludmylla/foodapi/domain/model/PhotoProduct.java
@@ -0,0 +1,33 @@
+package com.github.ludmylla.foodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class PhotoProduct {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "product_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Product product;
+
+    @Column(name = "name_file")
+    private String fileName;
+
+    private String description;
+
+    @Column(name = "content_type")
+    private String contentType;
+
+    @Column(name = "file_size")
+    private Long size;
+
+}
