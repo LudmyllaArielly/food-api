c0cb443bf557083327a01b4d49c38e12746ecd61
Ludmylla <ludmay4@gmail.com>
2022-09-19 19:50:11 -0300

    criando classe EmailProperties para acessar configurações do properties

---
+++ src/main/java/com/github/ludmylla/foodapi/core/email/EmailProperties.java
@@ -0,0 +1,20 @@
+package com.github.ludmylla.foodapi.core.email;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+
+@Validated
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("food.api.email")
+public class EmailProperties {
+
+    @NotNull
+    private String sender;
+}
