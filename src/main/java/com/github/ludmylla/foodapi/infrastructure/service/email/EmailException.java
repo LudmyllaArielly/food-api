0ffccad0c72591c61412f2b21bcfee18ade1a705
Ludmylla <ludmay4@gmail.com>
2022-09-19 19:48:44 -0300

    criando exceção emailException

---
+++ src/main/java/com/github/ludmylla/foodapi/infrastructure/service/email/EmailException.java
@@ -0,0 +1,13 @@
+package com.github.ludmylla.foodapi.infrastructure.service.email;
+
+public class EmailException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public EmailException(String message) {
+        super(message);
+    }
+
+    public EmailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
