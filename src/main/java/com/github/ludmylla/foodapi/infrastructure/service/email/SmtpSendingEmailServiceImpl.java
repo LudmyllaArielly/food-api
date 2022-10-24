2398e3aa858a56c9c564d980d6fae3878e591b87
Ludmylla <ludmay4@gmail.com>
2022-10-24 19:44:28 -0300

    adiciona método para envio de email

--- src/main/java/com/github/ludmylla/foodapi/infrastructure/service/email/SmtpSendingEmailServiceImpl.java
+++ src/main/java/com/github/ludmylla/foodapi/infrastructure/service/email/SmtpSendingEmailServiceImpl.java
@@ -2,11 +2,15 @@ package com.github.ludmylla.foodapi.infrastructure.service.email;
 
 import com.github.ludmylla.foodapi.core.email.EmailProperties;
 import com.github.ludmylla.foodapi.domain.service.SendingEmailService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 @Service
@@ -18,21 +22,42 @@ public class SmtpSendingEmailServiceImpl implements SendingEmailService {
     @Autowired
     private EmailProperties emailProperties;
 
+    @Autowired
+    private Configuration freemarkerConfig;
+
     @Override
     public void send(Message message) {
         try {
-
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
-            helper.setTo(message.getAddressees().toArray(new String [0]));
-            helper.setFrom(emailProperties.getSender());
-            helper.setSubject(message.getTopic());
-            helper.setText(message.getBody(), true);
-
+            MimeMessage mimeMessage = createMimeMessage(message);
             mailSender.send(mimeMessage);
-        } catch (Exception e) {
-            throw new EmailException("Could not send email.", e);
+        }catch (Exception ex){
+            throw new EmailException("Could not send email.", ex);
+        }
+    }
+
+    protected MimeMessage createMimeMessage(Message message) throws MessagingException {
+
+        String body = processTemplate(message);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+        helper.setFrom(emailProperties.getSender());
+        helper.setTo(message.getAddressees().toArray(new String[0]));
+        helper.setSubject(message.getTopic());
+        helper.setText(body, true);
+
+        return mimeMessage;
+    }
+
+    public String processTemplate(Message message) {
+        try {
+            Template template = freemarkerConfig.getTemplate(message.getBody());
+
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template, message.getVariables());
+
+        }catch (Exception ex) {
+            throw new EmailException("Could not mount email template.", ex);
         }
     }
 }
